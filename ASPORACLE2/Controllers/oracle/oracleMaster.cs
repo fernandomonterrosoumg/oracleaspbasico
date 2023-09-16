2aff4fafe160730847392ac12cd1f79732e26739
Fernando Monterroso <elmerfer_mejor@hotmail.com>
2023-09-15 18:54:33 -0600

    primera carga

---
+++ ASPORACLE2/Controllers/oracle/oracleMaster.cs
@@ -0,0 +1,148 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ASPORACLE2.Controllers.oracle
+{
+    public class oracleMaster
+    {
+
+        public static string CNNString()
+        {
+            string oradb = ConfigurationSettings.AppSettings["Oracle"];
+            return oradb;
+        }
+
+        public static DataTable ExecuteQuery(string sqlQuery, Dictionary<string, object> parameters = null)
+        {
+            using (OracleConnection connection = new OracleConnection(CNNString()))
+            {
+                connection.Open();
+
+                using (OracleCommand command = new OracleCommand(sqlQuery, connection))
+                {
+                    if (parameters != null)
+                    {
+                        foreach (var param in parameters)
+                        {
+                            command.Parameters.Add(new OracleParameter(param.Key, param.Value));
+                        }
+                    }
+
+                    using (OracleDataAdapter adapter = new OracleDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        return dataTable;
+                    }
+                }
+            }
+        }
+
+        public static int ExecuteNonQuery(string sqlQuery, Dictionary<string, object> parameters = null)
+        {
+            using (OracleConnection connection = new OracleConnection(CNNString()))
+            {
+                connection.Open();
+
+                using (OracleCommand command = new OracleCommand(sqlQuery, connection))
+                {
+                    if (parameters != null)
+                    {
+                        foreach (var param in parameters)
+                        {
+                            //command.Parameters.Add(new OracleParameter(param.Key, param.Value));
+                            command.Parameters.Add(new OracleParameter
+                            {
+                                ParameterName = param.Key,
+                                Value = param.Value,
+                                OracleDbType = GetOracleDbType(param.Value)// Asigna el tipo de parámetro correcto
+                            });
+                        }
+                    }
+
+                    return command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static OracleDbType GetOracleDbType(object valor)
+        {
+            OracleDbType oracleDbType = OracleDbType.Varchar2;
+
+            if (valor is DateTime)
+            {
+                oracleDbType = OracleDbType.Date;
+            }
+            else if (valor is long)
+            {
+                oracleDbType = OracleDbType.Int64;
+            }
+            else if (valor is int)
+            {
+                oracleDbType = OracleDbType.Int32;
+            }
+            else if (valor is short)
+            {
+                oracleDbType = OracleDbType.Int16;
+            }
+            else if (valor is sbyte)
+            {
+                oracleDbType = OracleDbType.Byte;
+            }
+            else if (valor is byte)
+            {
+                oracleDbType = OracleDbType.Int16;
+            }
+            else if (valor is decimal)
+            {
+                oracleDbType = OracleDbType.Decimal;
+            }
+            else if (valor is float)
+            {
+                oracleDbType = OracleDbType.Single;
+            }
+            else if (valor is double)
+            {
+                oracleDbType = OracleDbType.Double;
+            }
+            else if (valor is byte[])
+            {
+                oracleDbType = OracleDbType.Blob;
+            }
+
+            return oracleDbType;
+        }
+
+
+        public static void LogError()
+        {
+            string DefaultLogPath = "Logs";
+            string DefaultErrorMessage = "An error occurred.";
+            string logFormat = $"{DateTime.Now:dd/MM/yyyy hh:mm:ss tt} ==> ";
+            string logName = $"{DateTime.Now:dd-MM-yyyy tt} ==> ";
+            string errorTime = DateTime.Now.ToString("yyyy-MM-dd");
+
+            string logFilePath = Path.Combine(DefaultLogPath, $"{errorTime}-masterclass.txt");
+            string fullLogPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, logFilePath);
+
+            Directory.CreateDirectory(Path.GetDirectoryName(fullLogPath));
+
+            using (StreamWriter sw = new StreamWriter(fullLogPath, true))
+            {
+                sw.WriteLine(logFormat + DefaultErrorMessage);
+            }
+        }
+
+
+    }
+
+
+
+
+}
---
+++ ASPORACLE2/Default.aspx.cs
@@ -0,0 +1,73 @@
+﻿using ASPORACLE2.Controllers.oracle;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPORACLE2
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Definir la consulta SQL de actualización
+                string query = @"select * from tb_sexoss where CODIGO=:codigo";
+
+                // Crear un diccionario de parámetros con los nuevos valores y condiciones
+                Dictionary<string, object> parameters = new Dictionary<string, object>();
+                parameters.Add(":codigo", 1);
+
+                // Ejecutar la actualización
+                DataTable rowsAffected = oracleMaster.ExecuteQuery(query, parameters);
+
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error " + ex.Message);
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Definir la consulta SQL de actualización
+                string query = @"UPDATE tb_sexo SET ABREVIATURA = :ABREVIATURA
+                        WHERE codigo = :codigo";
+
+                // Crear un diccionario de parámetros con los nuevos valores y condiciones
+                Dictionary<string, object> parameters = new Dictionary<string, object>();
+                parameters.Add(":ABREVIATURA", "X");
+                parameters.Add(":codigo", 1);
+
+                // Ejecutar la actualización
+                int rowsAffected = oracleMaster.ExecuteNonQuery(query, parameters);
+
+                if (rowsAffected > 0)
+                {
+                    Console.WriteLine("Actualización exitosa.");
+                }
+                else
+                {
+                    Console.WriteLine("La actualización no tuvo éxito.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error durante la actualización: " + ex.Message);
+            }
+        }
+
+    }
+}
